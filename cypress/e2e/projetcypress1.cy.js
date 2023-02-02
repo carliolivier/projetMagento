e8f58e02044d8641c0d579fa5feb3e5d34b52f0f
carliolivier <olivier.carlier.foreach@gmail.com>
2023-02-02 10:44:10 +0100

    projet

--- cypress/e2e/projetcypress1.cy.js
+++ cypress/e2e/projetcypress1.cy.js
@@ -28,19 +28,25 @@ describe("automatisation d'un terst", () => {
 	});
 
 	it("addtocart1", () => {
-		cy.get('#ui-id-4').trigger('mouseover');
-		cy.get('#ui-id-9').trigger('mouseover');
-		cy.get('#ui-id-11 > span').click();
-		cy.get('.wrapper > .products > :nth-child(1)').trigger('mouseover');
-		cy.get('.swatch-opt-1396 > .size > .swatch-attribute-options > #option-label-size-143-item-167').click();
-		cy.get('.swatch-opt-1396 > .swatch-attribute.color > .swatch-attribute-options > #option-label-color-93-item-49').click();
+		cy.get("#ui-id-4").trigger("mouseover");
+		cy.get("#ui-id-9").trigger("mouseover");
+		cy.get("#ui-id-11 > span").click();
+		cy.get(".wrapper > .products > :nth-child(1)").trigger("mouseover");
+		cy.get(
+			".swatch-opt-1396 > .size > .swatch-attribute-options > #option-label-size-143-item-167"
+		).click();
+		cy.get(
+			".swatch-opt-1396 > .swatch-attribute.color > .swatch-attribute-options > #option-label-color-93-item-49"
+		).click();
 		cy.get('[class="action tocart primary"]').first().click({ force: true });
 		cy.wait(3000);
-		cy.get('.showcart').click();
+		cy.get(".showcart").click();
 		cy.wait(3000);
-		cy.get('#top-cart-btn-checkout').click();
-		cy.get('#customer-email-fieldset > .required').type("darkvador@yopmail.com");
-        cy.get('[name="shippingAddress.firstname"]').type("street of tarkov");
+		cy.get("#top-cart-btn-checkout").click();
+		cy.get("#customer-email-fieldset > .required").type(
+			"darkvador@yopmail.com"
+		);
+		cy.get('[name="shippingAddress.firstname"]').type("street of tarkov");
 		cy.get('[name="shippingAddress.lastname"]').type("tarkov");
 		cy.get('[name="shippingAddress.company"]').type("wild");
 		cy.get('[name="shippingAddress.street.0"]').type("1 rue de lille");
@@ -48,11 +54,50 @@ describe("automatisation d'un terst", () => {
 		cy.get('[class="select"]').eq(0).select("Alaska");
 		cy.get('[name="shippingAddress.postcode"]').type("59000");
 		cy.get('[class="select"]').eq(1).select("France");
-        cy.get('[name="shippingAddress.telephone"]').type('0320123456');
-		cy.get(':nth-child(2) > :nth-child(1) > .radio').click();
-        cy.get('.button').click();
-		cy.get('#billing-address-same-as-shipping-checkmo').click();
-		cy.get('.payment-method-content > :nth-child(4) > div.primary > .action').click();
+		cy.get('[name="shippingAddress.telephone"]').type("0320123456");
+		cy.get(":nth-child(2) > :nth-child(1) > .radio").click();
+		cy.get(".button").click();
+		cy.get("#billing-address-same-as-shipping-checkmo").click();
+		cy.get(
+			".payment-method-content > :nth-child(4) > div.primary > .action"
+		).click();
+	});
+	it("modifier quantite panier", () => {
+		cy.get("#ui-id-4").trigger("mouseover");
+		cy.get("#ui-id-9").trigger("mouseover");
+		cy.get("#ui-id-11 > span").click();
+		cy.get(".wrapper > .products > :nth-child(1)").trigger("mouseover");
+		cy.get(
+			".swatch-opt-1396 > .size > .swatch-attribute-options > #option-label-size-143-item-167"
+		).click();
+		cy.get(
+			".swatch-opt-1396 > .swatch-attribute.color > .swatch-attribute-options > #option-label-color-93-item-49"
+		).click();
+		cy.get('[class="action tocart primary"]').first().click({ force: true });
+		cy.wait(2000);
+		cy.get(".showcart").click();
+		cy.wait(2000);
+		cy.get(":nth-child(7) > .secondary > .action > span").click();
 
+		cy.get('input[class="input-text qty"]').clear().type("20");
+		cy.get(".update > span").click();
+		cy.wait(1000);
+		cy.get('.checkout-methods-items > :nth-child(1) > .action > span').click();
+		cy.get("#customer-email-fieldset > .required").type("darkvador@yopmail.com");
+		cy.get('[name="shippingAddress.firstname"]').type("street of tarkov");
+		cy.get('[name="shippingAddress.lastname"]').type("tarkov");
+		cy.get('[name="shippingAddress.company"]').type("wild");
+		cy.get('[name="shippingAddress.street.0"]').type("1 rue de lille");
+		cy.get('[name="shippingAddress.city"]').type("Lille");
+		cy.get('[class="select"]').eq(0).select("Alaska");
+		cy.get('[name="shippingAddress.postcode"]').type("59000");
+		cy.get('[class="select"]').eq(1).select("France");
+		cy.get('[name="shippingAddress.telephone"]').type("0320123456");
+		cy.get(":nth-child(2) > :nth-child(1) > .radio").click();
+		cy.get(".button").click();
+		cy.get("#billing-address-same-as-shipping-checkmo").click();
+		cy.get(
+			".payment-method-content > :nth-child(4) > div.primary > .action"
+		).click();
 	});
 });
